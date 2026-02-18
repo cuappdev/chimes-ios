508776ce67c66b13edfe6fd87020509a94f46bf4
Jay Z. <jidong.1272@gmail.com>
2026-02-18 18:38:06 -0500

    progress reward system

---
+++ Chimes/Utils/DesignConstants.swift
@@ -0,0 +1,18 @@
+//
+//  DesignConstants.swift
+//  Chimes
+//
+//  Created by Jay on 2/18/26.
+//
+
+import SwiftUI
+
+struct DesignConstants {
+    
+    enum Fonts {
+        
+        
+        
+        
+    }
+}
