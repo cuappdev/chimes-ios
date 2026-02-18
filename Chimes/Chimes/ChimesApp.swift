e94754068525aa3282b6aff91edcede38496a576
Jay Z. <jidong.1272@gmail.com>
2026-02-18 16:52:06 -0500

    Initial commit

---
+++ Chimes/Chimes/ChimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChimesApp.swift
+//  Chimes
+//
+//  Created by Jay on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ChimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
